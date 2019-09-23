ed54ead343b4fb02418d5f34a33c93163a5f432b
최민지 <choimj0728@douzone.com>
2019-09-23 10:49:28 +0900

    add env

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 5000;
 app.get("/", function(req, res) {
-  res.send("Hello World! >>> heroku >>>>> cmj ver 0.1");
+  res.send("Hello World! >>> heroku >>>>> cmj ver 0.2");
 });
 
 app.listen(PORT, function() {
