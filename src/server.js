c8735f254157c9103ae3322fe8bdc8b1dbf72541
최민지 <choimj0728@douzone.com>
2019-09-23 10:57:58 +0900

    update nodemon

--- src/server.js
+++ src/server.js
@@ -1,10 +1,33 @@
-var express = require("express");
-var app = express();
+import { GraphQLServer } from "graphql-yoga";
+import schema from "./schema";
+import GoogleStrategy from "./auth/googleOauth";
+import JwtStrategy from "./auth/jwt";
+import jwt from "jsonwebtoken";
+import passport from "passport";
+import bodyParser from "body-parser";
+
+import dotenv from "dotenv";
+dotenv.config(); //.env 파일 로드
+
+const logger = require("morgan");
+
 const PORT = process.env.PORT || 5000;
-app.get("/", function(req, res) {
-  res.send("Hello World! >>> heroku >>>>> cmj ver 0.3");
+const server = new GraphQLServer({ schema });
+const jwtSecret = process.env.JWT_SECRET;
+
+passport.use(JwtStrategy);
+
+server.express.use(bodyParser.urlencoded({ extended: false }));
+server.express.use(bodyParser.json());
+
+server.express.use(passport.initialize()); // passport 구동
+server.express.use(passport.session()); // 세션 연결
+
+server.express.get("/", function(req, res) {
+  res.send("Hello World! >>> heroku >>> ver 0.2");
 });
 
-app.listen(PORT, function() {
-  console.log(`Example app listening on port ${PORT}!`);
+server.express.use(logger("dev"));
+server.start({ port: PORT }, () => {
+  console.log(`========>> Server running on http://localhost:${PORT}`);
 });
