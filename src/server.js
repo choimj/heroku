977b64fd7051ae300f0f83b3bd35ebc8d1ea9549
최민지 <choimj0728@douzone.com>
2019-09-23 14:24:21 +0900

    heroku

--- src/server.js
+++ src/server.js
@@ -61,19 +61,14 @@ server.express.get(
     /**
      * google API로 인증받은 후 front-end 로 controll 넘겨서 처리
      */
-    // const { flag, data } = req.user;
-    // // console.log(req.user);
-    // if (flag) {
-    //   // res.redirect("http://localhost:3000/callback/login?email=" + data.email);
-    //   res.redirect(
-    //     "https://rbsdemo.netlify.com/callback/login?email=" + data.email
-    //   );
-    // } else {
-    //   // res.redirect("http://localhost:3000/callback/join?email=" + data.email);
-    //   res.redirect(
-    //     "https://rbsdemo.netlify.com/callback/login?email=" + data.email
-    //   );
-    // }
+    const url = process.env.HEROKU_URL + "/callback/login?email=" + data.email;
+    if (flag) {
+      // res.redirect("http://localhost:3000/callback/login?email=" + data.email);
+      res.redirect(url);
+    } else {
+      // res.redirect("http://localhost:3000/callback/join?email=" + data.email);
+      res.redirect(url);
+    }
   }
 );
 
@@ -99,10 +94,6 @@ server.express.post(
     return res.json({ flag: true, user: req.user });
   }
 );
-
-// server.express.get("/", function(req, res) {
-//   res.send("Hello World! >>> heroku >>> ver 0.2");
-// });
 
 // server.express.use(logger("dev"));
 server.start({ port: PORT }, () => {
